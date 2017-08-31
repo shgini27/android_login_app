9bb65fbffca95822750cb9a0e464a4388123e469
shgini27 <go_shuhrat@yahoo.com>
2017-08-31 11:45:41 +0500

    08-31-2017

---
+++ app/src/main/java/com/example/shagy/androidlogin/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.shagy.androidlogin.app;
+
+/**
+ * Created by Shagy on 7/14/2017.
+ */
+
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://192.168.100.125/android_login_api/login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://192.168.100.125/android_login_api/register.php";
+}
